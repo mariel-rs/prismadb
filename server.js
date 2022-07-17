1d7f757f9a88c5a605608992e6879c50a1a49b4a
mariel-rs <mariel.rsal@gmail.com>
2022-07-16 23:45:13 -0500

    Add endpoints for Details model

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
-    res.json({message: "alive"});
+    res.json({message: "alive"})
 });
-
-// Endpoints for the Explorer model
 
 app.get("/explorers", async (req, res) => {
     const allExplorers = await prisma.explorer.findMany({});
@@ -21,7 +19,7 @@ app.get("/explorers/:id", async (req, res) => {
     const id = parseInt(req.params.id);
     const explorer = await prisma.explorer.findUnique(
         {where: {id: id}}
-    );
+        );
     res.json(explorer);
 });
 
@@ -63,60 +61,6 @@ app.delete("/explorers/:id", async (req, res) => {
     return res.json({message: "Explorer eliminado correctamente"});
 });
 
-// Endpoints for the Details model
-
-app.get("/details", async (req, res) => {
-    const explorerDetails = await prisma.details.findMany({});
-    res.json(explorerDetails);
-});
-
-/*
-Crea un endpoint DELETE para eliminar un registro
-*/
-app.get("/details/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
-    const explorer = await prisma.details.findUnique({
-        where: {id: id}
-    });
-    res.json(explorer);
-});
-
-app.post("/details", async(req, res) => {
-    const explorer = {
-        name: req.body.name,
-        lang: req.body.lang,
-        missionCommander: req.body.missionCommander
-    };
-
-    const msg = "Explorer creado";
-    await prisma.details.create({data: explorer});
-    
-    return res.json({msg});
-});
-
-app.put("/details/:id", async(req, res) => {
-    const id = parseInt(req.params.id);
-
-    await prisma.details.update({
-        where: {
-            id: id
-        },
-        data: {
-            lang: req.body.lang
-        }
-    });
-    res.json({message: "Updated explorer details"});
-});
-
-app.delete("/details/:id", async(req, res) => {
-    const id = parseInt(req.params.id);
-
-    await prisma.details.delete({
-        where: {id: id}
-    });
-    res.json({message: "Deleted entry"});
-});
-
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`)
 });
